7ca925962cea6ff029095f5d874bd4e69ed135cc
Pedro-Juan-Bonilla <juan.bonilla13@upr.edu>
2025-04-19 15:42:09 -0400

    Optimized code in main from ifs to switch case, main code now catches error of input variety.

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.time.LocalTime;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
@@ -9,37 +10,34 @@ public class Main {
         System.out.println("Welcome back OPERATOR. For seeing the STATUS of VEHICLES enter 1, For seeing the RESERVATIONS enter 2, To CLOSE THE PROGRAM enter 3.");
         //TODO add posible errors (what if its is not a number, or a diferent number aka not 1,2 or 3)
         
-        int option = scan.nextInt(); 
+        int option = 0; 
+        
         while(option != 3) {
-            if(option != 1 && option != 2) {
-                System.out.println("PLEASE enter only the NUMBERS 1, 2 and 3.");
-            }
-
-            if(option == 1) {
-
-                //Enter into STATUS.
-
-                if(hours.getHour() < 7 || hours.getHour() >= 16){
-                System.out.println("Sorry, our rent system is only open from 7:00am-6:00pm.");
-                System.out.println(hours.getHour());
-                    //TODO add a way to close the program || inmediatly turn back to the option screen.
-
-                }
-
-                 
-            }
-
-            //Enters into RESERVATIONS.
-
-            if(option == 2) {
-
-                //does NOT check if services.
-
-
-                
-            }
-            
+            try{ // use a try to catch the error if operator inputs something that is not a number.
             option = scan.nextInt();
+            switch (option) {
+                case 1:
+                if(hours.getHour() < 7 || hours.getHour() >= 16){
+                    System.out.println("Sorry, our rent system is only open from 7:00am-6:00pm.");
+                    System.out.println(hours.getHour());
+                        //TODO add a way to close the program || inmediatly turn back to the option screen.
+                    }
+                    System.out.println("List of VEHICLES");
+                    break;
+                case 2: 
+
+                System.out.println("RESERVATIONS");
+                break;
+                case 3: break; //closer program/while loop.
+                default:
+                    System.out.println("ERROR: Please enter a VALID number (1, 2, or 3)."); //when error is wrong number input.
+                    break;
+            }
+        } catch (InputMismatchException e) {
+                System.out.println("ERROR: Please enter a valid NUMBER (1, 2, or 3)."); //when error is wrong input type.
+                scan.next(); // Clear the invalid input from the scanner.
+        }
+            
         }
         scan.close();
     }
