072b064a3cf25cb71b152fe60c344f81d4671f38
pedrosoler101 <pedro.soler@upr.edu>
2025-04-20 17:20:30 -0400

    Added classes for reservations and waitlist

--- src/information/Reservations.java
+++ src/information/Reservations.java
@@ -1,5 +1,85 @@
 package information;
 
+import java.time.LocalTime;
+import information.AvailableStations.LOCATION;
+
 public class Reservations {
-    
+    private int reservationNum;
+    private Users user;
+    private Vehicles vehicle;
+    private LOCATION pickupLocation;
+    private LOCATION dropoffLocation;
+    private LocalTime startTime;
+    private LocalTime endTime;
+    private int totalCredits;
+
+    public Reservations(int reservationNum, Users user, Vehicles vehicle, LOCATION pickupLocation, LOCATION dropoffLocation, LocalTime startTime, LocalTime endTime, int totalCredits){
+        this.reservationNum = reservationNum;
+        this.user = user;
+        this.vehicle = vehicle;
+        this.pickupLocation = pickupLocation;
+        this.dropoffLocation = dropoffLocation;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.totalCredits = totalCredits;
+    }
+
+    // Setters and Getters.
+
+    public int getReservationNum(){
+        return reservationNum;
+    }
+    public void setReservationNum(int ReservationNum){
+        this.reservationNum = ReservationNum;
+    }
+
+    public Users getUser(){
+        return user;
+    }
+    public void setUser(Users User){
+        this.user = User;
+    }
+
+    public Vehicles getVehicle(){
+        return vehicle;
+    }
+    public void setVehicle(Vehicles Vehicle){
+        this.vehicle = Vehicle;
+    }
+
+    public LOCATION getPickupLocation(){
+        return pickupLocation;
+    }
+    public void setPickupLocation(LOCATION PickupLocation){
+        this.pickupLocation = PickupLocation;
+    }
+
+    public LOCATION getDropoffLocation(){
+        return dropoffLocation;
+    }
+    public void setDropoffLocation(LOCATION DropoffLocation){
+        this.dropoffLocation = DropoffLocation;
+    }
+
+    public LocalTime getStartTime(){
+        return startTime;
+    }
+    public void setStartTime(LocalTime StartTime){
+        this.startTime = StartTime;
+    }
+
+    public LocalTime getEndTime(){
+        return endTime;
+    }
+    public void setEndTime(LocalTime EndTime){
+        this.endTime = EndTime;
+    }
+
+    public int getTotalCredits(){
+        return totalCredits;
+    }
+    public void setTotaCredits(int TotalCredits){
+        this.totalCredits = TotalCredits;
+    }
+
 }
--- src/information/Waitlist.java
+++ src/information/Waitlist.java
@@ -1,5 +1,35 @@
 package information;
 
+import java.util.LinkedList;
+import java.util.Queue;
+import information.AvailableStations.LOCATION;
+
 public class Waitlist {
+    private LOCATION location;
+    private Queue<Users> queue;
     
+    public Waitlist(LOCATION location){
+        this.location = location;
+        this.queue = new LinkedList<>();
+    }
+
+    public LOCATION getLocation(){
+        return location;
+    }
+
+    public void add(Users user){
+        queue.offer(user);
+    }
+
+    public Users next(){
+        return queue.poll();
+    }
+
+    public boolean isEmpty(){
+        return queue.isEmpty();
+    }
+
+    public Queue<Users> getQueue(){
+        return queue;
+    }
 }
