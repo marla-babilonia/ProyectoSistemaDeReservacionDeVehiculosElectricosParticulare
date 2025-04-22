7695693111825619d3dcf9967bcd5feac48d68f0
pedrosoler101 <pedro.soler@upr.edu>
2025-04-22 09:29:50 -0400

    Added and fixed waitlist class

---
+++ src/information/Waitlist.java
@@ -0,0 +1,85 @@
+package information;
+
+import java.time.LocalTime;
+import information.AvailableStations.LOCATION;
+
+public class Waitlist {
+    private int waitlistNum;
+    private Users user;
+    private Vehicles vehicle;
+    private LOCATION pickupLocation;
+    private LOCATION dropoffLocation;
+    private LocalTime startTime;
+    private LocalTime endTime;
+    private int totalCredits;
+
+    public Waitlist(int waitlistNum, Users user, Vehicles vehicle, LOCATION pickupLocation, LOCATION dropoffLocation, LocalTime startTime, LocalTime endTime, int totalCredits){
+        this.waitlistNum = waitlistNum;
+        this.user = user;
+        this.vehicle = vehicle;
+        this.pickupLocation = pickupLocation;
+        this.dropoffLocation = dropoffLocation;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.totalCredits = totalCredits;
+    }
+
+    // Setters and Getters.
+
+    public int getWaitlistNum(){
+        return waitlistNum;
+    }
+    public void setWaitlistNum(int WaitlistNum){
+        this.waitlistNum = WaitlistNum;
+    }
+
+    public Users getUser(){
+        return user;
+    }
+    public void setUser(Users User){
+        this.user = User;
+    }
+
+    public Vehicles getVehicle(){
+        return vehicle;
+    }
+    public void setVehicle(Vehicles Vehicle){
+        this.vehicle = Vehicle;
+    }
+
+    public LOCATION getPickupLocation(){
+        return pickupLocation;
+    }
+    public void setPickupLocation(LOCATION PickupLocation){
+        this.pickupLocation = PickupLocation;
+    }
+
+    public LOCATION getDropoffLocation(){
+        return dropoffLocation;
+    }
+    public void setDropoffLocation(LOCATION DropoffLocation){
+        this.dropoffLocation = DropoffLocation;
+    }
+
+    public LocalTime getStartTime(){
+        return startTime;
+    }
+    public void setStartTime(LocalTime StartTime){
+        this.startTime = StartTime;
+    }
+
+    public LocalTime getEndTime(){
+        return endTime;
+    }
+    public void setEndTime(LocalTime EndTime){
+        this.endTime = EndTime;
+    }
+
+    public int getTotalCredits(){
+        return totalCredits;
+    }
+    public void setTotaCredits(int TotalCredits){
+        this.totalCredits = TotalCredits;
+    }
+
+}
