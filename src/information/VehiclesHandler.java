08d2ca9434cd30d7af0189a43e03c557f060e97a
Pedro-Juan-Bonilla <juan.bonilla13@upr.edu>
2025-04-24 19:13:37 -0400

    Vehicels handler done!

--- src/information/VehiclesHandler.java
+++ src/information/VehiclesHandler.java
@@ -1,6 +1,65 @@
 package information;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class VehiclesHandler {
+  protected HashSet<Vehicles> availableVehicles = new HashSet<>();
+
+  
+    // Agregar vehículos iniciales si están disponibles
+    public void AvailableVehicles(Set<Vehicles> allVehicles) {
+      for (Vehicles v : allVehicles) {
+          if (v.getavailable()) {
+              availableVehicles.add(v);
+          }
+      }
+  }
+
+  // Actualizar disponibilidad de un vehículo
+  public void updateAvailability(Vehicles vehicle) {
+      if (vehicle.getavailable()) {
+          availableVehicles.add(vehicle);
+      } else {
+          availableVehicles.remove(vehicle);
+      }
+  }
+
+  // Buscar vehículo por ID
+  public Vehicles findById(int id) {
+      for (Vehicles v : availableVehicles) {
+          if (v.getID() == id) {
+              return v;
+          }
+      }
+      return null;
+  }
+
+  // Editar vehículo (por ejemplo, cambiar tipo o descripción)
+  public boolean editVehicle(int id, Vehicles.VEHICLE_TYPE Type, String Description) {
+      Vehicles v = findById(id);
+      if (v != null) {
+          v.setVehicleType(Type);
+          v.setdescription(Description);
+          return true;
+      }
+      return false;
+  }
+
+  // Remover vehículo manualmente
+  public boolean removeVehicle(int id) {
+      Vehicles exists = findById(id);
+      if (exists != null) {
+          availableVehicles.remove(exists);
+          return true;
+      }
+      return false;
+  }
+
+  // Obtener todos los vehículos disponibles
+  public Set<Vehicles> getAvailableVehicles() {
+      return availableVehicles;
+  }
   //metodo para crear set de available vehicles
 // editar el set available method para que si es falso quitarlo de la lista y si es cierto anadirlo a la lista
 
